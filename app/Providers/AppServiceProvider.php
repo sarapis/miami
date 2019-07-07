768107969a8871214f1639ca88f52c6aec31de49
Milos <grantlatest@gmail.com>
2019-07-07 20:46:50 +0200

    search

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,59 +21,59 @@ class AppServiceProvider extends ServiceProvider
             $view->with('organizations', $organizations);
         });
 
-        view()->composer('layouts.sidebar', function($view)
-        {
-            $taxonomies = \App\Taxonomy::where('taxonomy_parent_name', '=', NULL)->orderBy('taxonomy_name', 'asc')->get();
-            $view->with('taxonomies', $taxonomies);
-        });
+        // view()->composer('layouts.sidebar', function($view)
+        // {
+        //     $taxonomies = \App\Taxonomy::where('taxonomy_parent_name', '=', NULL)->orderBy('taxonomy_name', 'asc')->get();
+        //     $view->with('taxonomies', $taxonomies);
+        // });
 
-        view()->composer('layouts.sidebar', function($view)
-        {
-            $ages = \App\Detail::where('detail_type', '=', 'Ages Served')->orderBy('detail_value', 'asc')->get();
-            $view->with('ages', $ages);
-        });
+        // view()->composer('layouts.sidebar', function($view)
+        // {
+        //     $ages = \App\Detail::where('detail_type', '=', 'Ages Served')->orderBy('detail_value', 'asc')->get();
+        //     $view->with('ages', $ages);
+        // });
 
-        view()->composer('layouts.sidebar', function($view)
-        {
-            $languages = \App\Detail::where('detail_type', '=', 'Language(s)')->orderBy('detail_value', 'asc')->get();
-            $view->with('languages', $languages);
-        });
+        // view()->composer('layouts.sidebar', function($view)
+        // {
+        //     $languages = \App\Detail::where('detail_type', '=', 'Language(s)')->orderBy('detail_value', 'asc')->get();
+        //     $view->with('languages', $languages);
+        // });
 
-        view()->composer('layouts.sidebar', function($view)
-        {
-            $service_settings = \App\Detail::where('detail_type', '=', 'Service Setting')->orderBy('detail_value', 'asc')->get();
-            $view->with('service_settings', $service_settings);
-        });
+        // view()->composer('layouts.sidebar', function($view)
+        // {
+        //     $service_settings = \App\Detail::where('detail_type', '=', 'Service Setting')->orderBy('detail_value', 'asc')->get();
+        //     $view->with('service_settings', $service_settings);
+        // });
 
-        view()->composer('layouts.sidebar', function($view)
-        {
-            $insurances = \App\Detail::where('detail_type', '=', 'insurance')->orderBy('detail_value', 'asc')->get();
-            $view->with('insurances', $insurances);
-        });
+        // view()->composer('layouts.sidebar', function($view)
+        // {
+        //     $insurances = \App\Detail::where('detail_type', '=', 'insurance')->orderBy('detail_value', 'asc')->get();
+        //     $view->with('insurances', $insurances);
+        // });
 
-        view()->composer('layouts.sidebar', function($view)
-        {
-            $culturals = \App\Detail::where('detail_type', '=', 'Cultural Competencies')->orderBy('detail_value', 'asc')->get();
-            $transportations = \App\Detail::where('detail_type', '=', 'Transportation')->orderBy('detail_value', 'asc')->get();
+        // view()->composer('layouts.sidebar', function($view)
+        // {
+        //     $culturals = \App\Detail::where('detail_type', '=', 'Cultural Competencies')->orderBy('detail_value', 'asc')->get();
+        //     $transportations = \App\Detail::where('detail_type', '=', 'Transportation')->orderBy('detail_value', 'asc')->get();
 
-            $view->with('culturals', $culturals);
-        });
+        //     $view->with('culturals', $culturals);
+        // });
 
-        view()->composer('layouts.sidebar', function($view)
-        {
+        // view()->composer('layouts.sidebar', function($view)
+        // {
 
-            $transportations = \App\Detail::where('detail_type', '=', 'Transportation')->orderBy('detail_value', 'asc')->get();
+        //     $transportations = \App\Detail::where('detail_type', '=', 'Transportation')->orderBy('detail_value', 'asc')->get();
 
-            $view->with('transportations', $transportations);
-        });
+        //     $view->with('transportations', $transportations);
+        // });
 
-        view()->composer('layouts.sidebar', function($view)
-        {
+        // view()->composer('layouts.sidebar', function($view)
+        // {
 
-            $hours = \App\Detail::where('detail_type', '=', 'Additional Hours')->orderBy('detail_value', 'asc')->get();
+        //     $hours = \App\Detail::where('detail_type', '=', 'Additional Hours')->orderBy('detail_value', 'asc')->get();
 
-            $view->with('hours', $hours);
-        });
+        //     $view->with('hours', $hours);
+        // });
 
         view()->composer('layouts.script', function($view)
         {
