043756cb4d637a486710d44ec0d84441830ef0b1
loveisbluenova <alekssoftdev@gmail.com>
2019-10-22 20:38:59 -0400

    updated result

--- resources/views/frontEnd/service.blade.php
+++ resources/views/frontEnd/service.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title')
-{{$service->service_name}}
+
 @stop
 {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> --}}
 
