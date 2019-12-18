274fc9e0fa15836ca6392083330a530b83e97a11
loveisbluenova <alekssoftdev@gmail.com>
2019-12-18 08:54:36 -0500

    cron job for datasysnc

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('App\Http\Controllers\UploadController@zip_cron')->daily();
+        $schedule->call('App\Http\Controllers\UploadController@zip_cron')->everyMinute();
         // $schedule->command('inspire')
         //          ->hourly();
     }
