df168362277bbc0c0f413879a63b479ec8ba6324
loveisbluenova <alekssoftdev@gmail.com>
2020-01-04 17:49:31 -0500

    added put some comments in export page

--- resources/views/backEnd/pages/export.blade.php
+++ resources/views/backEnd/pages/export.blade.php
@@ -26,10 +26,26 @@ Export
             <div class="form-horizontal form-label-left">
               <div class="item form-group">
                 <a class="btn btn-primary" href="/export_hsds_zip_file">Download HSDS Zip File</a>
-           <!--      <button class="btn btn-primary"><a href="/zip_export/datapackage.zip" style="color: white;" download>Download HSDS Zip File</a></button> -->
+           <!--      <button class="btn btn-primary"><a href="/zip_export/datapackage.zip" style="color: white;" download>Download HSDS Zip File</a></button> -->          
               </div>
             </div>
 
+            <div class="form-horizontal form-label-left">
+              <div class="item form-group">
+                <h5>
+                  The downloaded HSDS Zip file will be available to use as a import file for other project with API format like below:
+                </h5>
+                <p>  
+                  http://23.96.85.224/datapackages?auth_key={key}
+                </p>
+                <h5>
+                  The request method is "GET" and key info will be provided by administrator.
+                </h5>
+                <p for="datapackages_url_example" style="font-style: italic; color: grey;">   Example:
+                  <a href="http://23.96.85.224/datapackages?auth_key=aksflak601KKKSS1050A0A" style="color: #027bff;"> http://23.96.85.224/datapackages?auth_key=aksflak601KKKSS1050A0A</a>
+                  </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
